8c44a0fa8294e64c353b632fbb4c48fe2bd67943
jrdchenga <147221313+jrdchenga@users.noreply.github.com>
2024-03-19 11:57:56 -0400

    Create SWITCH conditionals

--- vscode/script.js
+++ vscode/script.js
@@ -1,6 +1,68 @@
-// Make the choose random:
-const generateTarget = () => {
-    return targetNumber = Math.floor(Math.random() * 20);
-  };
-  generateTarget();
-  console.log(generateTarget())
+const getComputerChoice = computerNumber => {
+  computerNumber = Math.floor(Math.random() * 20);
+  
+  switch (computerNumber) {
+    case 0:
+      return "";
+      break;
+    case 1:
+      return "";
+      break;
+    case 2:
+      return "";
+      break;
+    case 3:
+      return "";
+      break;
+    case 4:
+      return "";
+      break;
+    case 5:
+      return "";
+      break;
+    case 6:
+      return "";
+      break;
+    case 7:
+      return "";
+      break;
+    case 8:
+      return "";
+      break;
+    case 0:
+      return "";
+      break;
+    case 10:
+      return "";
+      break;
+    case 11:
+      return "";
+      break;
+    case 12:
+      return "";
+      break;
+    case 13:
+      return "";
+      break;
+    case 14:
+      return "";
+      break;
+    case 15:
+      return "";
+      break;
+    case 16:
+      return "";
+      break;
+    case 17:
+      return "";
+      break;
+    case 18:
+      return "";
+      break;
+    case 19:
+      return "";
+      break;
+    default:
+      return "invalid";
+  }
+}
