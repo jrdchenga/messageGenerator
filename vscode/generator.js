3377171bd55b01b46f19cf0de9caf5257335952c
jrdchenga <147221313+jrdchenga@users.noreply.github.com>
2024-03-30 00:54:20 -0400

    Final commit

--- vscode/generator.js
+++ vscode/generator.js
@@ -1,9 +1,11 @@
 let message;
 
-const messageGeneratorDisplay = document.getElementById('message');
+const messageGeneratorDisplay = document.getElementById('Message');
 const generateButton = document.getElementById('generate');
 
 generateButton.addEventListener('click', () => {
     message = generateMessage(computerNumber);
     messageGeneratorDisplay.innerHTML = message;
+    messageGeneratorDisplay.style.marginLeft = 'auto';
+    messageGeneratorDisplay.style.color = 'black';
 })
